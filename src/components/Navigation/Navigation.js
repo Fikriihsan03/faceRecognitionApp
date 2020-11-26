c12799980fe591a294cb2e1d59e9f6fcdce86b21
fikri <mfikriihsan03@gmail.com>
2020-11-26 18:09:41 +0700

    initial commit

---
+++ src/components/Navigation/Navigation.js
@@ -0,0 +1,8 @@
+const Navigation= () => {
+    return(
+        <nav style={{display : 'flex',justifyContent:'flex-end'}}>
+            <p className ="underline pointer f3 link dim ph3 pv2 mb2 dib white bg-mid-gray">Sign out</p>
+        </nav>
+    )
+    }
+    export default Navigation;
